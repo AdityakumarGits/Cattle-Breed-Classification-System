346940d4df24d610e90e5ca8600649b9a931f671
Aditya Kumar <adityaraj12.ap@gmail.com>
2026-06-06 23:51:43 +0530

    Add prediction debug logs

--- cattle-backend/config/mail.js
+++ cattle-backend/config/mail.js
@@ -1,17 +1,17 @@
-
 import dotenv from "dotenv";
 dotenv.config();
 
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
-
-
 
 export default transporter;
